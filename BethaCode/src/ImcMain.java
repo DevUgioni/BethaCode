f3cc77c381a7944ae2e37702c7b86aa400e86b33
DevUgioni <ugioni.dav@gmail.com>
2021-07-07 21:44:49 -0300

    Adicionado ImcMain, conforme solicitado

---
+++ BethaCode/src/ImcMain.java
@@ -0,0 +1,29 @@
+public class ImcMain {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.nome = "Davi Ugioni";
+        pessoa1.cpf = "192168125";
+        pessoa1.altura = 1.70;
+        pessoa1.peso = 61.9;
+        pessoa1.filiacao = "Mãe Teste";
+
+        pessoa1.getIMC();
+        System.out.println(pessoa1.nome+ ", peso: " +pessoa1.peso+ " e altura " +pessoa1.altura+ " seu IMC é: "+pessoa1.IMC);
+    }
+
+    static class Pessoa {
+        String nome;
+        String cpf;
+        String rg;
+        Double altura;
+        Double peso;
+        String dataNascimento;
+        String filiacao;
+        Double IMC;
+
+        void getIMC(){
+            IMC = peso / Math.pow(altura,2);
+        }
+    }
+}
